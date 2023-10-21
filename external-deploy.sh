4bc0516a14ca1c73957acf0ea5342cf5ba8fdb71
angryrobotdeals <121421580+angryrobotdeals@users.noreply.github.com>
2023-10-22 00:25:17 +0300

    Stage #3.3

---
+++ external-deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ -f .env ]; then
+  export $(cat .env | xargs)
+else
+  echo "Файл .env не найден"
+  exit 1
+fi
+
+
+ssh -i $APP_SERVER_SSH_KEY $APP_SERVER_USER << "EOF"
+
+# Переходим в директорию с репозиторием
+cd $APP_SERVER_PATH
+pwd
+
+# Останавливаем контейнеры
+docker compose -p cc -f docker-compose.yml down --remove-orphans
+
+# REMOVE ALL UNUSED DATA
+docker image prune -f -a
+docker volume prune -f
+
+# Стягиваем последние изменения из репозитория
+git reset --hard
+git checkout main
+git reset --hard
+git pull
+
+# Поднимаем контейнеры с последними изменениями
+docker compose -p cc -f docker-compose.yml build
+docker compose --env-file .env -p cc -f docker-compose.yml up -d --remove-orphans
+
+docker ps -s
+
+EOF
+
+# Сообщаем об успешном выполнении скрипта
+echo "APP CI Done!"
