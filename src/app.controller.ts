517660b1352fe5f305c543ffd9ffb88bcd49f739
angryrobotdeals <121421580+angryrobotdeals@users.noreply.github.com>
2023-10-30 13:51:48 +0300

    Add getTopTradeCoins endpoint #1.3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,57 +1,53 @@
-import { Body, Controller, Get, Inject, Param, Post, Query, UseInterceptors } from '@nestjs/common';
-import { Exchange, Market } from '@prisma/client';
-import { CACHE_MANAGER, CacheInterceptor, CacheTTL } from '@nestjs/cache-manager';
-import { Cache } from 'cache-manager';
+import { Body, Controller, Get, Param, Post, Query, UseInterceptors } from '@nestjs/common';
+import { CacheInterceptor, CacheTTL } from '@nestjs/cache-manager';
 import { PrismaService } from './prisma.service';
 import { AppService } from './app.service';
 import { TIMEFRAME } from './timeseries.interface';
+import { Exchange as ExchangeModel, Market as MarketModel } from '@prisma/client';
 
 @Controller()
 export class AppController {
   constructor(
-    @Inject(CACHE_MANAGER) private cacheManager: Cache,
     private readonly appService: AppService,
     private readonly prisma: PrismaService,
   ) {}
 
-  @Get()
-  getHello(): string {
+  @Get() getHello(): string {
     return this.appService.getHello();
   }
 
   @Get('exchange')
-  async getAllExchanges(): Promise<Exchange[]> {
+  async getAllExchanges(): Promise<ExchangeModel[]> {
     return this.prisma.exchange.findMany();
   }
 
   @Get('market')
-  async getAllMarkets(): Promise<Market[]> {
+  async getAllMarkets(): Promise<MarketModel[]> {
     return this.prisma.market.findMany();
   }
 
   @Get('updateTopCoins')
-  async updateTopCoins(): Promise<Market[]> {
+  async updateTopCoins(): Promise<any[]> {
     return this.appService.updateTopCoins();
   }
 
   @Get('getTopCoins')
-  async getTopCoins(): Promise<Market[]> {
+  async getTopCoins(): Promise<any[]> {
     return this.appService.getTopCoins();
   }
 
   @CacheTTL(300000) // 5 minutes
   @UseInterceptors(CacheInterceptor)
   @Get('getATHL')
-  async getATHL(): Promise<Market[]> {
+  async getATHL(): Promise<any[]> {
     return this.appService.getATHL();
   }
 
   @CacheTTL(300000) // 5 minutes
   @UseInterceptors(CacheInterceptor)
-  @Get('getTopTradeCoins')
-  // show top coins by turnover in USD
+  @Get('getTopTradeCoins') // show top coins by turnover in USD
   // example: http://localhost:3000/getTopTradeCoins?turnover=1000000
-  async getTopTradeCoins(@Query('turnover') turnover: string): Promise<Market[]> {
+  async getTopTradeCoins(@Query('turnover') turnover: string): Promise<any[]> {
     return this.appService.getTopTradeCoins(+turnover || undefined);
   }
 
@@ -62,14 +58,7 @@ export class AppController {
 
   @Post('candle/download')
   async fetchCandles(
-    @Body()
-    body: {
-      exchange: string;
-      symbol: string;
-      timeframe: TIMEFRAME;
-      start: number;
-      limit: number;
-    },
+    @Body() body: { exchange: string; symbol: string; timeframe: TIMEFRAME; start: number; limit: number },
   ): Promise<any[] | string> {
     return this.appService.fetchCandles(body);
   }
