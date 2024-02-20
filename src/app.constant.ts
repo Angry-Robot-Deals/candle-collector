83f6e2fbbe2f68d0bebc756c638acb6c59064f3c
angryrobotdeals <121421580+angryrobotdeals@users.noreply.github.com>
2024-02-20 13:00:41 +0300

    fetchCandles fringe #2022.2

--- src/app.constant.ts
+++ src/app.constant.ts
@@ -1,4 +1,4 @@
-export const FETCH_DELAY: number = 1000 * 60 * 60 * 4; // 4 hours
+export const FETCH_DELAY: number = 1000 * 60 * 60 * 2; // 2 hours
 export const CALCULATE_ATHL_PERIOD: number = 1000 * 60 * 60; // 1 hour
 
 export const START_FETCH_TIME = new Date('2022-01-01T00:00:00Z');
