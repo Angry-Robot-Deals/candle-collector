cdf6f0662d6c687ad214eaa710715b3a79fe82a0
angryrobotdeals <121421580+angryrobotdeals@users.noreply.github.com>
2024-03-02 13:49:22 +0300

    fix deploy #1.1

--- scripts/external-deploy.sh
+++ scripts/external-deploy.sh
@@ -9,7 +9,7 @@ else
   exit 1
 fi
 
-scp -i "$APP_SERVER_SSH_KEY" ./.env.production "$APP_SERVER_USER":/repos/candle-collector/
+scp -i "$APP_SERVER_SSH_KEY" ./.env.production "$APP_SERVER_USER":/repos/candle-collector/.env
 
 ssh -i "$APP_SERVER_SSH_KEY" "$APP_SERVER_USER" << "EOF"
 
